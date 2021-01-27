830f559d49195e05f860267cc68dfe6ec7f65e88
wangzhiyao <wangzhiyao66@sina.com>
2021-01-27 14:54:38 +0800

    增加变量

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ This is a warehouse for beginners
 1.https://react.docschina.org/tutorial/tutorial.html
 
 2.https://www.jianshu.com/p/27d111a50a06
+
+3. study link:https://react.docschina.org/docs/introducing-jsx.html
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -3,6 +3,12 @@ require("./home.css");
 import React from "react";
 const name = "Josh Perez";
 class HomeComponent extends React.Component {
+
+  const user = {
+    firstName: 'Harper',
+    lastName: 'Perez'
+  };
+
   formatName(user) {
     return user.firstName + ' ' + user.lastName;
   }
@@ -11,8 +17,8 @@ class HomeComponent extends React.Component {
     return (
       <div className="home">
         <div className="children-one">
-          {" "}
-          我是一个 子级 div 哈 ！！！名字叫做: {name}
+          {' '}
+          我是一个 子级 div 哈 ！！！名字叫做: {name} 
         </div>
       </div>
     );
