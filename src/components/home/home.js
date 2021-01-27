20c4012b0a6657b6aaa373c848f92b8509d3f42b
王治尧 <1217167460@qq.com>
2021-01-27 22:36:11 +0800

    基础语法学习01

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -14,13 +14,31 @@ class HomeComponent extends React.Component {
       firstName: "Harper",
       lastName: "Perez"
     },
-    class:"children-one" // 属性绑定语法
+    class: "children-one" // 属性绑定语法
   }
 
   // 定义 名字拼接 方法
   formatName(user) {
-    
     return user.firstName + ' + ' + user.lastName;
+  }
+
+  // 创建时钟
+  createTime() {
+    return <span> {new Date().toLocaleTimeString()} </span>
+  }
+
+  // 每秒变更一次
+  changTime() {
+    setInterval(this.createTime(), 1000);
+  }
+
+  // demo  函数式组件
+  Clock(props) {
+    return (
+      <div>
+        函数式组件 {props.date.toLocaleTimeString()}
+      </div>
+    );
   }
 
   // 渲染函数 调用 渲染页面内容
@@ -28,13 +46,19 @@ class HomeComponent extends React.Component {
     return (
       <div className="home">
         {/* 属性绑定语法 */}
-        <div className={this.state.class} >
-          我是一个 子级 div 哈 ！！！名字叫做: { this.formatName(this.state.users)}
+        <div className={this.state.class} tabIndex="0">
+          我是一个 子级 div 哈 ！！！名字叫做: {this.formatName(this.state.users)}, It is {this.createTime()}
+          <p>props : {this.props.name}</p>
         </div>
+
       </div>
     );
   }
 }
 
-HomeComponent.defaultProps = {};
+HomeComponent.defaultProps = {
+  props: {
+    name: 'project name is test !'
+  }
+};
 export default HomeComponent;
