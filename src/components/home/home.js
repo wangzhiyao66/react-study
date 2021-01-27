41ab90aec3e6daabc86a6757ad80371926608faa
wangzhiyao <wangzhiyao66@sina.com>
2021-01-27 19:14:50 +0800

    变量声明 函数使用 属性绑定

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,29 +1,35 @@
 require("./home.css");
 
 import React from "react";
+
+// 静态常量 使用
 // const name = "Josh Perez";
+
+// study link: https://react.docschina.org/docs/introducing-jsx.html
 class HomeComponent extends React.Component {
 
-  // const user = {
-  //   firstName: 'Harper',
-  //   lastName: 'Perez'
-  // };
+  // 定义静态数据变量
   state = {
     users: {
       firstName: "Harper",
       lastName: "Perez"
-    }
+    },
+    class:"children-one" // 属性绑定语法
   }
 
+  // 定义 名字拼接 方法
   formatName(user) {
-    return user.firstName + ' ' + user.lastName;
+    
+    return user.firstName + ' + ' + user.lastName;
   }
 
+  // 渲染函数 调用 渲染页面内容
   render() {
     return (
       <div className="home">
-        <div className="children-one">
-          我是一个 子级 div 哈 ！！！名字叫做: { this.state.users.lastName}
+        {/* 属性绑定语法 */}
+        <div className={this.state.class} >
+          我是一个 子级 div 哈 ！！！名字叫做: { this.formatName(this.state.users)}
         </div>
       </div>
     );
