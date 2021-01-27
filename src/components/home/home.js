c9bb1149fd34309ae7c75708f9d8a52e17652c90
jing <xudajing1994@163.com>
2021-01-27 16:19:26 +0800

    change test

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,13 +1,19 @@
 require("./home.css");
 
 import React from "react";
-const name = "Josh Perez";
+// const name = "Josh Perez";
 class HomeComponent extends React.Component {
 
-  const user = {
-    firstName: 'Harper',
-    lastName: 'Perez'
-  };
+  // const user = {
+  //   firstName: 'Harper',
+  //   lastName: 'Perez'
+  // };
+  state = {
+    users: {
+      firstName: "Harper",
+      lastName: "Perez"
+    }
+  }
 
   formatName(user) {
     return user.firstName + ' ' + user.lastName;
@@ -17,8 +23,7 @@ class HomeComponent extends React.Component {
     return (
       <div className="home">
         <div className="children-one">
-          {' '}
-          我是一个 子级 div 哈 ！！！名字叫做: {name} 
+          我是一个 子级 div 哈 ！！！名字叫做: { this.state.users.lastName}
         </div>
       </div>
     );
