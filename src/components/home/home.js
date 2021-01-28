438c11f32806ddc9dcc7b13a318f1446ed859911
jing <xudajing1994@163.com>
2021-01-28 13:16:12 +0800

    添加方法

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -23,7 +23,10 @@ class HomeComponent extends React.Component {
     return (
       <div className="home">
         <div className="children-one">
-          我是一个 子级 div 哈 ！！！名字叫做: { this.state.users.lastName}
+          我是一个 子级 div 哈 ！！！名字叫做: {this.state.users.lastName}
+        </div>
+        <div>
+        方法调用:{this.formatName(this.state.users)}
         </div>
       </div>
     );
