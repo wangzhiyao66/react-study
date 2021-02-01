0c23782658a6da2b7ad6241e7ee139102d98256e
wangzhiyao <wangzhiyao66@sina.com>
2021-02-01 18:42:58 +0800

    form new project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/Main';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-// Render the main component into the dom
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
