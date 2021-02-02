3a6ac2a3dab993958b5a4f7aea009fe62799e14d
wangzhiyao <wangzhiyao66@sina.com>
2021-02-02 11:01:55 +0800

    菜单分栏处理

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,7 +17,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <div className="sidebar">sidebar</div>
+      <div className="content">content</div>
     </div>
   );
 }
